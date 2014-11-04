393fe286954030e70c26719eab70ef8ac668bf57
Ludovic Borie <borie.l@sfeir.com>
2014-11-04 23:49:43 +0100

    Fetch forecast

--- app/src/main/java/org/lille/gdg/sunshine/MainActivity.java
+++ app/src/main/java/org/lille/gdg/sunshine/MainActivity.java
@@ -1,19 +1,9 @@
 package org.lille.gdg.sunshine;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Adapter;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-
-import java.util.ArrayList;
-import java.util.List;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -24,7 +14,7 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
+                    .add(R.id.container, new ForecastFragment())
                     .commit();
         }
     }
@@ -50,46 +40,5 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-
-            List<String> fakeContent = new ArrayList<String>();
-            fakeContent.add("foo");
-            fakeContent.add("zlfkzlj");
-            fakeContent.add("tonton");
-            fakeContent.add("tata");
-            fakeContent.add("tata");
-            fakeContent.add("tata");
-            fakeContent.add("tata");
-            fakeContent.add("tata");
-            fakeContent.add("tata");
-            fakeContent.add("tata");
-            fakeContent.add("tata");
-            fakeContent.add("tata");
-            fakeContent.add("tata");
-            fakeContent.add("tata");
-            fakeContent.add("tata");
-            fakeContent.add("tata");
-            fakeContent.add("tutu");
-            fakeContent.add("toto");
-            fakeContent.add("bar");
-
-            ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(getActivity(), R.layout.list_item_forecat, R.id.list_item_forecast_textview, fakeContent);
-            ((ListView)rootView.findViewById(R.id.listview_forecast)).setAdapter(arrayAdapter);
-
-            return rootView;
-        }
     }
 }
